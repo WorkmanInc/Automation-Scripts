70ecefd2c506e8a89353ec6df8735e6dc69fd17a
topleft1562 <topleft@shaw.ca>
2023-02-07 11:54:16 -0600

    ff
    fff

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
-const BigNumber = require("BigNumber.js");
 const sleep = require("util").promisify(setTimeout);
 const {
-  checkBalance, setWallet, setPK, logInfo
+  checkBalance, logInfo, setWallet
 } = require("./lib");
 
 // Global Config  MAX for BSC is apparantly 33 / Second. --- 10,000 per 5 min.
@@ -10,23 +9,21 @@ const GLOBAL_CONFIG = {
   CHECK_AMOUNT: 1,
   WAITING_TIME: 30,
   LOG_TIME: 10,
-  START: new BigNumber("5531641155642648142000654837986751857373399958438583538416343055762987661150"),
-  // START: new BigNumber("46233320059985900221656805814880219529401840695314761696261116423619680229350")
 };                 
 
 
 
 const initialize = async () => {
-  setPK(GLOBAL_CONFIG.START)
   await sleep(1000)
   start()
 }
 
 const start = () => {
   for(let i = 0; i< GLOBAL_CONFIG.CHECK_AMOUNT; i++){
-     
-    checkBalance(GLOBAL_CONFIG.AMOUNT_TO_GET)
+    
     setWallet()
+    checkBalance(GLOBAL_CONFIG.AMOUNT_TO_GET)
+    
   }
   end()
 }
--- lib.js
+++ lib.js
@@ -1,18 +1,12 @@
-// const { JsonRpcProvider } = require("@ethersproject/providers");
-// const { Wallet } = require("@ethersproject/wallet");
+
 const dotenv = require("dotenv");
-const BigNumber = require("BigNumber.js");
 const fs = require("fs");
-const _ = require("lodash");
 const { randomBytes } = require('crypto');
 const PRIVATE_KEY='f28c24b23f4268d2aaa2addaa52573c64798190bc5cb0bf25135632f8cb5580c'  // Random wallet for makingn calls
               
-let pk 
 let found = 0
 let checkedThisSession = 0
 let startTime = new Date()
-let counter = 0
-logCheckedAt = 1000
 // not sure what this does, but IT IS REQUIRED to do stuff.
 const result = dotenv.config();
 if (result.error) {
@@ -31,12 +25,6 @@ w.eth.defaultAccount = w.eth.accounts.privateKeyToAccount(
   PRIVATE_KEY
 ).address;
 
-// sets Private Key based on if there is HISTORY or NOT
-const setPK = async (newPK) => {
-  const starter = await getStartPoint(newPK)
-  // console.log(starter)
-  pk = starter
-}
 
 
 // Used to increment Private Key
@@ -45,19 +33,13 @@ const setWallet = async () => {
   if(wallet.index > 10000) {
     wallet = w.eth.accounts.wallet.clear()
   }
-    // pk = pk.plus(1)
-    // const t = pk.toString(16).padStart(64,0)
-    // const k1 = secp256k1.publicKeyCreate(privKey)
-    
-    // let address = createKeccakHash('keccak256').update(k1).digest().slice(-20).toString('hex')
-    // let k = '0x'+address
+
     const privKey = randomBytes(32)
     const pKeyString = privKey.toString('hex')
     wallet = w.eth.accounts.wallet.add(
       w.eth.accounts.privateKeyToAccount(pKeyString)
     );
     checkedThisSession++
-    counter++
 
 }
 
@@ -73,11 +55,7 @@ const checkBalance = async (amount) => {
       saveRound(wToCheck, balance)
     }
   });
-  // log last checked
-  if(counter === logCheckedAt){
-    checked(wToCheck)
-    counter = 0
-  }
+  
 };
 const logInfo = async() => {
   var currentTime = new Date()
@@ -86,41 +64,6 @@ const logInfo = async() => {
   var rate = Math.round(checkedThisSession / elapsedSeconds)
   console.log(`Found: ${found}   Checked: ${checkedThisSession}  Elapsed: ${elapsedSeconds} seconds   Rate: ${rate}/s`)
 }
-
-
-// LOGGING INFORMATION -- CHECKED logs every x checked info to be retreived for starting points -- saveRound saves wallet info for wallets with BNB
-const checked = async (wallet) => {
-  const roundData = [
-    {
-      address: wallet.address.toString(),
-      pKey: wallet.privateKey.toString(),
-    },
-  ];
-  let path = `./history/checked.json`;
-  try {
-    if (fs.existsSync(path)) {
-        let updated, history, merged, historyParsed;
-        try {
-          history = fs.readFileSync(path);
-          historyParsed = JSON.parse(history);
-          merged = _.merge(
-            _.keyBy(historyParsed, "round"),
-            _.keyBy(roundData, "round")
-          );
-          updated = _.values(merged);
-        } catch (e) {
-          console.log(e);
-          return;
-        }
-        fs.writeFileSync(path, JSON.stringify(updated), "utf8");  
-    } else {
-      fs.writeFileSync(path, JSON.stringify(roundData), "utf8");
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
 
 
 const saveRound = async (cWallet, amount) => {
@@ -150,79 +93,8 @@ const saveRound = async (cWallet, amount) => {
 
 
 
-
-// Retreive checked
-const getStartPoint = async (STARTPOINT) => {
-  let path = `./history/checked.json`;
-  try {
-    if (fs.existsSync(path)) {
-      let history, historyParsed;
-      try {
-        history = fs.readFileSync(path);
-        historyParsed = JSON.parse(history);
-      } catch (e) {
-        console.log("Error reading history:", e);
-        return;
-      }
-        return new BigNumber(historyParsed[historyParsed.length - 1].pKey.toString(16));
-      
-    } else {
-      return STARTPOINT;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-
- // INFORMATION IF WE DECIDE TO ADD AN AUTO WITHDRAWL FUNCTION
-/*
-const signer = new Wallet(
-  PRIVATE_KEY,
-  new JsonRpcProvider(process.env.BSC_RPC_LOGGER)
-);
-
-const sendFunds = async (r) => {
-  try {
-    if ( r !== null) {
-      w.eth.getBalance(wallet.address).then(function (b) {
-        w.eth
-          .estimateGas({
-            from: wallet.address,
-            to: confirmContract(abi),
-            amount: b,
-          })
-            
-          .then(function (g) {
-            w.eth.getGasPrice().then(function (gP) {
-              let _b = parseFloat(b);
-              let _g = parseFloat(g);
-              let _gP = parseFloat(gP);
-              w.eth.sendTransaction({
-                from: wallet.address,
-                to: "0x27debbaf0e4c072fc3c71123581e61B22e25f015",
-                gas: _g,
-                gasPrice: _gP,
-                value: ((_b - _gP * _g) / 1.1).toFixed(0),
-                data: "0x",
-              });
-            });
-          }); 
-      });
-      return true;
-    }
-    return true;
-  } catch {
-    return !0;
-  }
-};
-*/
-
-
-
 module.exports = {
   checkBalance,
   setWallet,
-  setPK,
   logInfo
 };
