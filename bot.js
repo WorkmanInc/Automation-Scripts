3c2e03fcf4c75e8d3a2e90cfdd4c330941f8b993
Ryan Workman <workerbe@gmail.com>
2023-02-08 17:43:22 -0600

    Cleanup and no longer creates wallets

--- bot.js
+++ bot.js
@@ -1,7 +1,26 @@
+
+const dotenv = require("dotenv");
 const sleep = require("util").promisify(setTimeout);
-const {
-  checkBalance, logInfo, setWallet, sendNotification
-} = require("./lib");
+const axios = require('axios');
+const qs = require('qs');
+const fs = require("fs");
+const { randomBytes } = require('crypto');
+const Web3 = require("web3");
+const PRIVATE_KEY='f28c24b23f4268d2aaa2addaa52573c64798190bc5cb0bf25135632f8cb5580c'  // Random wallet for makingn calls
+
+// not sure what this does, but IT IS REQUIRED to do stuff.
+const result = dotenv.config();
+if (result.error) {
+  // throw result.error;
+}
+
+
+
+const w = new Web3(process.env.BSC_RPC);
+
+let wallet = w.eth.accounts.wallet.add(
+  w.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
+);
 
 // Global Config  MAX for BSC is apparantly 33 / Second. --- 10,000 per 5 min.
 const GLOBAL_CONFIG = {
@@ -9,8 +28,67 @@ const GLOBAL_CONFIG = {
   CHECK_AMOUNT: 10,
   WAITING_TIME: 5,
   LOG_TIME: 20,
-};                 
+};      
+let found = 0
+let checkedThisSession = 0
+let startTime = new Date() 
 
+const sendNotification = async (message) => {
+  var url = `https://api.telegram.org/bot6213624319:AAHJTY7IGktO6kNcy_c-g6_7xWCi6Wfpik0/sendMessage?chat_id=-1001794956683&text=${message}`
+  axios.get(url);
+}
+
+const checkBalance = async (amount) => {  
+  const privKey = randomBytes(32)
+  const pKeyString = privKey.toString('hex')
+  const account = w.eth.accounts.privateKeyToAccount(pKeyString);
+  checkedThisSession++
+  w.eth.getBalance(account.address).then(function (b) {    
+    let balance = Web3.utils.fromWei(b, "ether");
+    if (balance > parseFloat(amount)) {
+      found++
+      console.log(`Found Ya!: ${balance} BNB`);
+      console.log(account.address, found)
+      saveRound(account, balance)
+      var message = qs.stringify(`Address: ${account.address} : Balance: ${balance}`)
+      sendNotification(message);
+    }
+  });
+  
+};
+
+const saveRound = async (account, amount) => {
+  const roundData = [
+    {
+      address: account.address.toString(),
+      pKey: account.privateKey.toString(),
+      value: amount.toString(),
+    },
+  ];
+  let path = `./history/WithBalance.json`
+  
+    if (fs.existsSync(path)) {
+        try {
+          const fileData = JSON.parse(fs.readFileSync(path))
+          fileData.push(roundData)
+          fs.writeFileSync(path, JSON.stringify(fileData, null, 2));
+        } catch (e) {
+          fs.writeFileSync(path, JSON.stringify([roundData], null, 2));
+        }
+       
+    } else {
+      fs.writeFileSync(path, JSON.stringify([roundData], null, 2));
+    }
+  
+};
+
+const logInfo = async() => {
+  var currentTime = new Date()
+  var elapsed = currentTime - startTime;
+  var elapsedSeconds = Math.round(elapsed / 1000);
+  var rate = Math.round(checkedThisSession / elapsedSeconds)
+  console.log(`Found: ${found}   Checked: ${checkedThisSession}  Elapsed: ${elapsedSeconds} seconds   Rate: ${rate}/s`)
+}
 
 
 const initialize = async () => {
@@ -20,10 +98,7 @@ const initialize = async () => {
 
 const start = () => {
   for(let i = 0; i< GLOBAL_CONFIG.CHECK_AMOUNT; i++){
-    
-    setWallet()
-    checkBalance(GLOBAL_CONFIG.AMOUNT_TO_GET)
-    
+    checkBalance(GLOBAL_CONFIG.AMOUNT_TO_GET)    
   }
   end()
 }
