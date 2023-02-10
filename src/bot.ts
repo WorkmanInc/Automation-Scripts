4083a9ca69d1f89ce72168c6f371de99e05f646c
topleft1562 <topleft@shaw.ca>
2023-02-10 08:46:52 -0600

    ff
    ff

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,5 @@
 
 import dotenv from "dotenv";
-import axios from "axios";
 import util from "util"
 const sleep = util.promisify(setTimeout);
 import fetch from 'cross-fetch'
@@ -13,23 +12,13 @@ import { randomBytes} from "crypto";
 import Web3 from "web3";
 
 
-// const fetch = require("cross-fetch");
-// const { JsonRpcProvider } = require("@ethersproject/providers");
-// const { Wallet } = require("@ethersproject/wallet");
-// const { Contract } = require("ethers");
-// const BigNumber = require("BigNumber.js");
+const PRIVATE_KEY='af5b1f35d2ff08ac13746155fc3401aba64d8456a62655fec3d5b8e23a53c6c8'  // FARM
 
 
-const PRIVATE_KEY='f28c24b23f4268d2aaa2addaa52573c64798190bc5cb0bf25135632f8cb5580c'  // Random wallet for makingn calls
-// const abi = require("./abi.json");
-// Global Config  MAX for BSC is apparantly 33 / Second. --- 10,000 per 5 min.
 const GLOBAL_CONFIG = {
-  MINBUY: 0,
-  LP: "0x90E6b9C1e54d00b1766fcbD0Fa83B18349016217",
-  TOKEN0: "0x4130A6f00bb48ABBcAA8B7a04D00Ab29504AD9dA", //CIC
-  TOKEN1: "0x9a37bF232466a55B99428479dF22c049cD43c20E", //FRTc
-  CHATID: "-1001435750887",
-  PERDOT: 5 // $ value per DOT
+  CHECKEVERY: 300000,
+  TIMEAFTER: 120000,
+  KEEPER: "0x86D0c640E9B208acB39b04Bf5aAB1C41070632E3",
 };
 
 
@@ -39,6 +28,12 @@ if (result.error) {
   // throw result.error;
 }
 
+const w = new Web3(process.env.BSC_RPC!!);
+
+w.eth.defaultAccount = w.eth.accounts.privateKeyToAccount(
+  PRIVATE_KEY
+).address;
+
 
 const signer = new Wallet(
   PRIVATE_KEY,
@@ -47,19 +42,17 @@ const signer = new Wallet(
 
 
 let contract = new Contract(
-  GLOBAL_CONFIG.LP,
+  GLOBAL_CONFIG.KEEPER,
   abi,
   signer
 );
+const keeperContract = contract.connect(signer);
 
-
-      
-
-
-const sendNotification = async (message: string) => {
-  var url = `https://api.telegram.org/bot${process.env.BOT_TOKEN2}/sendMessage?chat_id=${GLOBAL_CONFIG.CHATID}&text=${message}`
-  axios.get(url);
-}
+const checkIfReady = async () => {
+  const {upkeepNeeded} = await keeperContract.upKeepDue();
+  console.log(upkeepNeeded)
+  return upkeepNeeded
+}   
 
 const getBNBPrice = async () => {
   const apiUrl = "https://backend.newscan.cicscan.com/coin_price";
@@ -77,43 +70,32 @@ const getBNBPrice = async () => {
   return 0
 };
 
-const calculate = async (cicP: number, Ain: BigNumber, Aout: BigNumber) => {
- 
-  const bought = await new BigNumber(Aout.toString())
-  const FRTcValue = await  new BigNumber(Ain.toString()).dividedBy(Aout.toString()).multipliedBy(cicP).toFixed(5)
- 
-  return { bought, FRTcValue }
+const runKeeper = async () => {
+  try {
+    const tx = await keeperContract.manualUpkeep();
+    await tx.wait();
+    console.log(`🤞 Ran 🍁`);
+     
+  } catch (error) {
+    console.log("Maybe Wait Longer??");
+   
+  }
 }
 
-const sym = (cicSpent: string | number) => {
-  const howMany = new BigNumber(cicSpent).toNumber()
-  let dots = "\xF0\x9F\x92\xB2"
-  if(howMany > 1){
-    for(let i=1; i<howMany; i++){
-      dots = dots + "\xF0\x9F\x92\xB2"
-    }
+const goIdle = async () => {
+  await sleep(GLOBAL_CONFIG.CHECKEVERY)
+  start()
+}
+
+
+const start = async () => {
+  const isReady = await checkIfReady()
+  if(isReady) {
+    runKeeper()
   }
-  return dots
+  goIdle()
 }
 
 console.log("Loaded Up!")
-
-contract.on("Swap", async (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
-  const cicPrice = await getBNBPrice()
-  const {bought, FRTcValue} = await calculate(cicPrice, amount0In, amount1Out)
-  const spent = new BigNumber(amount0In.toString()).shiftedBy(-18).multipliedBy(cicPrice).toFixed(2)
-  const dots = sym(new BigNumber(spent).dividedBy(GLOBAL_CONFIG.PERDOT).toFixed(0))
-  if( bought.gt(0) ) {
-    var message = 
-    "FRTc - Purchased!\n" +
-    dots +
-    `\nSpent: $${spent} - (${new BigNumber(amount0In.toString()).shiftedBy(-18).toFixed(2)} CIC)\n` +
-    `Received ${bought.shiftedBy(-18).toFixed(2)} FRTc\n` +
-    `FRTc Price: $${FRTcValue}\n` +
-    `CIC: $${cicPrice}\n`
-    
-    sendNotification(message);
-  }
-
-});
+start()
 
