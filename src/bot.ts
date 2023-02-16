cb7bf2c8e3d524c8db3b157b99771e4572317924
topleft1562 <topleft@shaw.ca>
2023-02-16 12:40:36 -0600

    ff
    ff

--- src/bot.ts
+++ src/bot.ts
@@ -6,16 +6,26 @@ import { JsonRpcProvider } from '@ethersproject/providers'
 import { Wallet } from '@ethersproject/wallet'
 import { Contract } from 'ethers'
 import abi from "./abi.json"
-import Web3 from "web3";
+// import Web3 from "web3";
 
 
 const PRIVATE_KEY='af5b1f35d2ff08ac13746155fc3401aba64d8456a62655fec3d5b8e23a53c6c8'  // FARM
-const CIC_RPC="https://xapi.cicscan.com/"
-
 
 const GLOBAL_CONFIG = {
   CHECKEVERY: 60000,
-  KEEPER: "0x86D0c640E9B208acB39b04Bf5aAB1C41070632E3",
+  CHAIN:[
+    {
+      NAME: "CIC",
+      KEEPER: "0x86D0c640E9B208acB39b04Bf5aAB1C41070632E3",
+      RPC: "https://xapi.cicscan.com/",
+    },
+    {
+      NAME: "BSC",
+      KEEPER: "0x1Bf307E0E5520eD98274BF9270167DA2acc8f044",
+      RPC: "https://bsc-dataseed1.binance.org/"
+    }
+  ]
+
 };
 
 
@@ -24,34 +34,42 @@ const result = dotenv.config();
 if (result.error) {
   // throw result.error;
 }
-
+/*
 const w = new Web3(CIC_RPC);
 
 w.eth.defaultAccount = w.eth.accounts.privateKeyToAccount(
   PRIVATE_KEY
 ).address;
+*/
+
+const getContract = (index: number) => {
+  const RPC = GLOBAL_CONFIG.CHAIN[index].RPC
+  const signer = new Wallet(
+    PRIVATE_KEY,
+    new JsonRpcProvider(RPC)
+  );
+  
+  let contract = new Contract(
+    GLOBAL_CONFIG.CHAIN[index].KEEPER,
+    abi,
+    signer
+  );
+  
+  const keeperContract = contract.connect(signer);
+  return keeperContract
+}
 
 
-const signer = new Wallet(
-  PRIVATE_KEY,
-  new JsonRpcProvider(CIC_RPC)
-);
 
-
-let contract = new Contract(
-  GLOBAL_CONFIG.KEEPER,
-  abi,
-  signer
-);
-const keeperContract = contract.connect(signer);
-
-const checkIfReady = async () => {
-  const {upkeepNeeded} = await keeperContract.upKeepDue();
-  return upkeepNeeded
+const checkIfReady = async (index: number) => {
+    const keeperContract = await getContract(index)
+    const {upkeepNeeded} = await keeperContract.upKeepDue();
+    return upkeepNeeded
 }   
 
-const runKeeper = async () => {
+const runKeeper = async (index: number) => {
   try {
+    const keeperContract = await getContract(index)
     await keeperContract.manualUpkeep();
     console.log("performing upkeep")
   } catch (error) {
@@ -66,12 +84,14 @@ const goIdle = async () => {
 
 
 const start = async () => {
-  const isReady = await checkIfReady()
-  console.log(isReady)
-  if(isReady) {
-    runKeeper()
+  for(let c=0; c<GLOBAL_CONFIG.CHAIN.length; c++){
+    const isReady = await checkIfReady(c)
+    if(isReady) {
+      console.log(`${GLOBAL_CONFIG.CHAIN[c].NAME} is Ready!`)
+      runKeeper(c)
+    }
+    goIdle()
   }
-  goIdle()
 }
 
 console.log("Loaded Up!")
