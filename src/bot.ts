8079f19d0104ba47a7f5540d41f396d3563c927d
topleft1562 <topleft@shaw.ca>
2023-02-17 09:25:00 -0600

    ff
    ff

--- src/bot.ts
+++ src/bot.ts
@@ -9,7 +9,12 @@ import abi from "./abi.json"
 import lotteryabi from "./lottery.json"
 import tokenabi from "./token.json"
 import BigNumber from "bignumber.js"
+import telegramBot from "node-telegram-bot-api"
 // import Web3 from "web3";
+
+// const token = "6131657839:AAHwkVz6Oy8OJL0sa3KuvERVCZZdRBgbMiY"   // PRODUCTION
+const token = "5721237869:AAE2ChqcZnjo8e18JaL7XmsvrbbSpFh8H04"   // testing
+const bot = new telegramBot(token, {polling: true})
 
 
 const PRIVATE_KEY='af5b1f35d2ff08ac13746155fc3401aba64d8456a62655fec3d5b8e23a53c6c8'  // FARM
@@ -45,7 +50,7 @@ w.eth.defaultAccount = w.eth.accounts.privateKeyToAccount(
 ).address;
 */
 
-const getContract = (index: number) => {
+const getKeeperContract = (index: number) => {
   const RPC = GLOBAL_CONFIG.CHAIN[index].RPC
   const signer = new Wallet(
     PRIVATE_KEY,
@@ -97,7 +102,7 @@ const getTokenContract = (index: number, address: string) => {
 }
 
 const checkIfReady = async (index: number) => {
-    const keeperContract = await getContract(index)
+    const keeperContract = await getKeeperContract(index)
 
     const {lottery, upkeepNeeded } = await keeperContract.upKeepDue();
     return { lottery, upkeepNeeded }
@@ -105,19 +110,33 @@ const checkIfReady = async (index: number) => {
 
 const runKeeper = async (index: number) => {
   try {
-    const {lottery, keeperContract } = await getContract(index)
+    const {lottery, keeperContract } = await getKeeperContract(index)
     const { step } = await keeperContract.lotteries(lottery.toString())
     await keeperContract.manualUpkeep();
     console.log("performing upkeep")
     if(step === 2){
-      // get Token Name
+      // get Token Name 
       const lotteryContract = await getLotteryContract(index, lottery.toString())
       const token = await lotteryContract.cakeToken()
       const id = await lotteryContract.currentLotteryId()
+      const lastID = (id-1).toString()
       const tokenContract = getTokenContract(index, token.toString())
-      const name = await tokenContract.name()
       // get tickets sold and winner count
-      const lotteryInfo = await lotteryContract.viewLottery(id.toString())
+      const lotteryInfo = await lotteryContract.viewLottery(lastID)
+
+      const name = await tokenContract.name()
+      const finalNumber = lotteryInfo.finalNumber - 1000000
+      const ticketsSold = new BigNumber(lotteryInfo.firstTicketIdNextLottery.toString()).minus(lotteryInfo.firstTicketId.toString()).toString()
+      // send msg to bot
+      console.log(name, 
+        `\n1 Number ${lotteryInfo.countWinnersPerBracket[0].toString()}`,
+        `\n2 Number ${lotteryInfo.countWinnersPerBracket[1].toString()}`, 
+        `\n3 Number ${lotteryInfo.countWinnersPerBracket[2].toString()}`, 
+        `\n4 Number ${lotteryInfo.countWinnersPerBracket[3].toString()}`, 
+        `\n5 Number ${lotteryInfo.countWinnersPerBracket[4].toString()}`, 
+        `\n6 Number ${lotteryInfo.countWinnersPerBracket[5].toString()}`, 
+        `\n Tickets sold${ticketsSold}\n`, 
+        finalNumber)
       // send msg to bot
 
     }
