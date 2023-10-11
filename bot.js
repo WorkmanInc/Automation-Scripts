f8cd7a3f35f15348c7082b49098e4a58cee362dc
topleft1562 <87454455+topleft1562@users.noreply.github.com>
2023-10-11 12:28:20 -0500

    gg

--- bot.js
+++ bot.js
@@ -1245,7 +1245,7 @@ bot.onText(/^\?{2}(.+)/, async function(message, match) {
 
         const itemlist = []
         if (bitcoinData.length > 1 ){
-            itemlist.push([{"text": `Other Tokens`, "callback_data": 'CHECK'}])
+            itemlist.push([{"text": `Other Tokens`, "callback_data": 99}])
         }
           const reply_markup = {"inline_keyboard": itemlist}
           const opts = {
